13b11eb5eba8f3d02e74f79ea6a43d46bdf321f6
Federico Andrucci <federico.andrucci@gmail.com>
2021-11-17 23:40:39 +0100

    swipe mode

--- AR-Dice/Assets/Scripts/GameController.cs
+++ AR-Dice/Assets/Scripts/GameController.cs
@@ -8,20 +8,52 @@ public class GameController : MonoBehaviour {
     private ARSessionOrigin arOrigin;
     private ARRaycastManager arRaycastManager;
 
+    private SwipeModeController swipeModeController;
+    private FallingModeController fallingModeController;
+
     private List<GameObject> dice;
+    private GameObject instantiatedDie;
+
+    private int currentDie;
+    
 
     void Start() {
         arOrigin = FindObjectOfType<ARSessionOrigin>();
         arRaycastManager = FindObjectOfType<ARRaycastManager>();
 
+        currentDie = 0;
+
         dice = Container.instance.dice;
+        
+        swipeModeController = new SwipeModeController();
+        fallingModeController = new FallingModeController();
+
+        if (Container.instance.throwMode == ThrowMode.SWIPE_TO_THROW) {
+            instantiatedDie = Instantiate(dice[currentDie]);
+            swipeModeController.Die = instantiatedDie;
+        } else if (Container.instance.throwMode == ThrowMode.FALLING) {
+            
+        }
     }
     
     void Update() {
         if (Container.instance.throwMode == ThrowMode.SWIPE_TO_THROW) {
-            
+            if (!swipeModeController.IsThrowed) {
+                swipeModeController.UpdateDiePosition();
+            } else {
+                // pick and set
+                // check die result
+            }
+
+            if (swipeModeController.IsThrowable) {
+                swipeModeController.SwipeDie();
+            }
         } else if(Container.instance.throwMode == ThrowMode.FALLING) {
             
         }
+    }
+
+    private void SetupSwipeToThrow() {
+        instantiatedDie = Instantiate(dice[currentDie]);
     }
 }
--- AR-Dice/Assets/Scripts/SwipeModeController.cs
+++ AR-Dice/Assets/Scripts/SwipeModeController.cs
@@ -2,13 +2,105 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SwipeModeController  {
+public class SwipeModeController {
+
+    private GameObject _die;
+    private bool _isThrowed;
+    private bool _throwable;
+    private Rigidbody rigidbody;
+    private float throwForceY = 0.2f;
+    private float throwForceZ = 10f;
 
     public SwipeModeController() {
-        
+        this._isThrowed = false;
+        this._throwable = true;
     }
 
     public void SwipeDie() {
+        Vector3 startPosition = Vector3.zero;
+        Vector3 endPosition = Vector3.zero;
+        Vector3 direction = Vector3.zero;
+        Vector3 torque = Vector3.zero;
+        float touchStartTime = 0f;
+        float touchEndTime = 0f;
+        float timeInterval = 0f;
+        bool onTouchHold = false;
         
+        if (Input.touchCount > 0) {
+            Touch touch = Input.GetTouch(0);
+            
+            if(touch.phase == TouchPhase.Began) {
+                startPosition = touch.position;
+                touchStartTime = Time.time;
+
+                Ray ray = Camera.current.ScreenPointToRay(startPosition);
+                RaycastHit hitObject;
+
+                if (250 >= startPosition.x && startPosition.x <= 650 && 150 <= startPosition.y && startPosition.y <= 550) {
+                    onTouchHold = true;
+                    rigidbody.isKinematic = true;
+                }
+            }
+
+            if (onTouchHold && touch.phase == TouchPhase.Ended) {
+                onTouchHold = false;
+                
+                // Force Detected
+                touchEndTime = Time.time;
+                timeInterval = touchEndTime - touchStartTime;
+                endPosition = touch.position;
+                direction = endPosition - startPosition;
+                
+                // Random Torque
+                torque.x = Random.Range(-200, 200);
+                torque.y = Random.Range(-200, 200);
+                torque.z = Random.Range(-200, 200);
+                
+                // Camera Rotation and Reset
+                float xCameraRotation = Camera.current.transform.forward.x;
+                float zCameraRotation = Camera.current.transform.forward.z;
+                float xForce = (throwForceZ / timeInterval) * xCameraRotation;
+                float zForce = (throwForceZ / timeInterval) * zCameraRotation;
+                float yForce = throwForceY * direction.y;
+
+                rigidbody.isKinematic = false;
+                rigidbody.AddForce(xForce, yForce, zForce);
+                rigidbody.AddTorque(torque);
+
+                _isThrowed = true;
+                _throwable = false;
+            }
+        }
+    }
+
+    public void UpdateDiePosition() {
+        _die.transform.position = Camera.current.ViewportToWorldPoint(new Vector3(0.5f, 0.2f, 0.5f));
+        _die.transform.rotation = Camera.current.transform.rotation;
+    }
+
+    public GameObject Die {
+        get => _die;
+        set {
+            _die = value;
+            rigidbody = _die.GetComponent<Rigidbody>();
+            rigidbody.isKinematic = true;
+
+            Vector3 v = new Vector3(_die.transform.position.x, _die.transform.position.y, 1f);
+            _die.transform.position = v;
+        }
+    }
+
+    public bool IsThrowed {
+        get => _isThrowed;
+        set {
+            _isThrowed = value;
+        }
+    }
+
+    public bool IsThrowable {
+        get => _throwable;
+        set {
+            _throwable = value;
+        }
     }
 }
