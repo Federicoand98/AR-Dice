3997f32cceef67862bfcdee0b99b94b7376c179b
Noesh <56556129+Noesh@users.noreply.github.com>
2021-11-21 14:52:37 +0100

    Presets and table mode added to scene

--- AR-Dice/Assets/Scripts/Settings/SettingsController.cs
+++ AR-Dice/Assets/Scripts/Settings/SettingsController.cs
@@ -2,17 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SettingsController : MonoBehaviour
-{
+public class SettingsController : MonoBehaviour {
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
     }
 
