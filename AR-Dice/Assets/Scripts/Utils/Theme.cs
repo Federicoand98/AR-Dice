77953b9f6844afc0e8576ba01df8370dba1ffd0e
Noesh <56556129+Noesh@users.noreply.github.com>
2021-12-11 00:43:07 +0100

    lil improvements

--- AR-Dice/Assets/Scripts/Utils/Theme.cs
+++ AR-Dice/Assets/Scripts/Utils/Theme.cs
@@ -2,20 +2,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable] public class Theme {
-
-    private float _dieR;
-    private float _dieG;
-    private float _dieB;
-
-    private float _numbR;
-    private float _numbG;
-    private float _numbB;
-
+    private float _dieR, _dieG, _dieB;
+    private float _numbR, _numbG, _numbB;
+    
     public Theme() {
+        // Die color initialized to grey
         _dieR = .345f;
         _dieG = .345f;
         _dieB = .345f;
 
+        // Number color initialized to white
         _numbR = 1f;
         _numbG = 1f;
         _numbB = 1f;
